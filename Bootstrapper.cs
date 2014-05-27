af2d1a74dde2815244f19447ae9220bb229ebb6b
Kradek <kradekmfc@gmail.com>
2014-05-26 21:49:12 -0400

    initial commit

---
+++ Bootstrapper.cs
@@ -0,0 +1,22 @@
+﻿using Nancy;
+using Nancy.Bootstrapper;
+using Nancy.TinyIoc;
+
+namespace ModelsOnline
+{
+    public class Bootstrapper : DefaultNancyBootstrapper
+    {
+        // The bootstrapper enables you to reconfigure the composition of the framework,
+        // by overriding the various methods and properties.
+        // For more information https://github.com/NancyFx/Nancy/wiki/Bootstrapper
+        protected override void RequestStartup(TinyIoCContainer requestContainer, IPipelines pipelines, NancyContext context)
+        {
+            pipelines.OnError += (ctx, ex) =>
+            {
+                return null;
+            };
+
+            base.RequestStartup(requestContainer, pipelines, context);
+        }
+    }
+}
---
+++ Modules/IndexModule.cs
@@ -0,0 +1,52 @@
+﻿using Nancy;
+using MFCChatClient;
+using System;
+using System.Linq;
+using Nancy.Json;
+using System.Net.Sockets;
+
+namespace ModelsOnline.Modules
+{
+    public class IndexModule : NancyModule
+    {
+        public IndexModule()
+        {
+            Get["/"] = parameters =>
+            {
+                var mfc = new MFCClient();
+                JsonSettings.MaxJsonLength = Int32.MaxValue;
+
+                Exception socketError = null ;
+                mfc.SocketError += (object s, SocketErrorEventArgs e) =>
+                {
+                    socketError = e.Exception;
+                };
+
+                var processed = false;
+                mfc.UsersProcessed += (s, e) =>
+                {
+                    processed = true;
+                };
+
+                var now = DateTime.Now;
+                var timeout = now.AddSeconds(10);
+                while (!processed && DateTime.Now < timeout && null == socketError) { };
+
+                if (null != socketError)
+                    return Response.AsJson(new { error = "Socket exception.", exception = socketError });
+                if (!processed)
+                    return Response.AsJson(new { error = "Timed out." });
+
+                var res = mfc.Models.Select(x => new
+                {
+                    name = x.Name,
+                    broadcasterId = x.UserId,
+                    state = x.VideoState.ToString(),
+                    camscore = x.ModelDetails.Camscore
+                }).OrderByDescending(y => y.camscore);
+
+                return Response.AsJson(res);
+            };
+        }
+    }
+}
